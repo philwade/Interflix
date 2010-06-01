274b584634cb4f89e3aa1eaad4f1f095437bc374
phil <phil@6596c492-bec3-4f50-a8a2-b0ff2d2738bf>
2010-06-01 21:13:27 +0000

    Pulling data from web
    git-svn-id: http://svn.philwade.org/phil/trunk/InterFlix@83 6596c492-bec3-4f50-a8a2-b0ff2d2738bf

--- src/phil/interflix/QueList.java
+++ src/phil/interflix/QueList.java
@@ -2,9 +2,10 @@ package phil.interflix;
 
 import android.app.ListActivity;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,11 +14,12 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
-import oauth.signpost.OAuthConsumer;
-import oauth.signpost.basic.DefaultOAuthConsumer;
+//import oauth.signpost.OAuthConsumer;
+//import oauth.signpost.basic.DefaultOAuthConsumer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -38,20 +40,27 @@ public class QueList extends ListActivity {
 			u = new URL("http://api.netflix.com/catalog/titles/autocomplete?oauth_consumer_key=zksyhhsj8uk85ckxpxurfw4v&term=arrested");
 			request = (HttpURLConnection) u.openConnection();
 			request.connect();
-			Document doc = loadXMLFromString((String)request.getContent());
+			Document doc = loadXMLFromConnection(request);
 			String[] titles = nodeListToArray(doc.getElementsByTagName("title"));
 			setListAdapter(new ArrayAdapter<String>(this, R.layout.que, titles));
     	} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
+    		//catches from new URL()
+			Toast.makeText(getApplicationContext(), "MalformedURLException up in here", 200).show();
+			setListAdapter(new ArrayAdapter<String>(this, R.layout.que, FAILURE));
 			e.printStackTrace();
 		}
 		catch (IOException e) {
+			//catches from URL.openConnection()
 			// TODO Auto-generated catch block
+			Toast.makeText(getApplicationContext(), "IOException", 200).show();
+			setListAdapter(new ArrayAdapter<String>(this, R.layout.que, FAILURE));
 			e.printStackTrace();
 		}
 		catch (Exception e){
+			Toast.makeText(getApplicationContext(), "General Exception up in here", 200).show();
 			e.printStackTrace();
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.que, COUNTRIES));
+			setListAdapter(new ArrayAdapter<String>(this, R.layout.que, FAILURE));
 		}
 		
 
@@ -73,68 +82,37 @@ public class QueList extends ListActivity {
 		}
     };
     
-    public static Document loadXMLFromString(String xml) throws Exception
+    public static Document loadXMLFromConnection(HttpURLConnection connection) throws Exception
     {
+    	BufferedReader in = new BufferedReader(
+    	new InputStreamReader(connection.getInputStream()));
+    	String inputLine;
+    	StringBuilder xml = new StringBuilder();
+
+    	while ((inputLine = in.readLine()) != null) 
+    		xml.append(inputLine);
+
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
-        InputSource is = new InputSource(new StringReader(xml));
+        InputSource is = new InputSource(new StringReader(xml.toString()));
         return builder.parse(is);
     }
     
     public String[] nodeListToArray(NodeList list)
     {
     	int length = list.getLength();
-    	String[] arr = null;
-    	for(int i = list.getLength(); i < length;i++)
+    	String[] arr = new String[length];
+    	for(int i = 0; i < length;i++)
     	{
-    		arr[i] = list.item(i).getNodeValue();
+    		NamedNodeMap attributes = list.item(i).getAttributes();
+    		arr[i] = attributes.getNamedItem("short").getNodeValue();
     	}
     	
     	return arr;
     }
 
-    static final String[] COUNTRIES = new String[] {
-    	"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
-        "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",
-        "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
-        "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
-        "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
-        "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory",
-        "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
-        "Cote d'Ivoire", "Cambodia", "Cameroon", "Canada", "Cape Verde",
-        "Cayman Islands", "Central African Republic", "Chad", "Chile", "China",
-        "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo",
-        "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic",
-        "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
-        "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
-        "Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands", "Fiji", "Finland",
-        "Former Yugoslav Republic of Macedonia", "France", "French Guiana", "French Polynesia",
-        "French Southern Territories", "Gabon", "Georgia", "Germany", "Ghana", "Gibraltar",
-        "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau",
-        "Guyana", "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong", "Hungary",
-        "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica",
-        "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos",
-        "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
-        "Macau", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
-        "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Moldova",
-        "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia",
-        "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand",
-        "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea", "Northern Marianas",
-        "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru",
-        "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar",
-        "Reunion", "Romania", "Russia", "Rwanda", "Sqo Tome and Principe", "Saint Helena",
-        "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon",
-        "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Saudi Arabia", "Senegal",
-        "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands",
-        "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "South Korea",
-        "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden",
-        "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "The Bahamas",
-        "The Gambia", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
-        "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Virgin Islands", "Uganda",
-        "Ukraine", "United Arab Emirates", "United Kingdom",
-        "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan",
-        "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna", "Western Sahara",
-        "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"
+    static final String[] FAILURE = new String[] {
+    	"Failed to load",
       };
 
 }
